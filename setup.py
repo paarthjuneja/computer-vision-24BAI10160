8d2649736e55bc26442077f23b976d41f65e55df
Paarth Juneja <paarthjuneja2006@gmail.com>
2026-09-18 22:27:15 +0530

    Initial setup with project files and gitignore

---
+++ setup.py
@@ -0,0 +1,10 @@
+﻿from setuptools import setup, find_packages
+
+setup(
+    name="visionforge",
+    version="0.1.0",
+    description="Computer Vision Project for Coursework",
+    author="Paarth Juneja",
+    author_email="paarthjuneja2006@gmail.com",
+    packages=find_packages(),
+)
